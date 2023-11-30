ed2ad54877fa9266a399e9769db7acff9cd2b7e4
markheinze <64999025+markheinze@users.noreply.github.com>
2023-11-29 19:43:30 -0500

    Create Frog.cs

---
+++ Packages/Frog.cs
@@ -0,0 +1,22 @@
+    public class FrogObject{
+      public string name;
+      public string abilityOne;
+      public string abilityTwo;
+      public string abilityThree;
+      public string abilityFour;
+      public string hat;
+      public string armor;
+      public string weapon;
+      public Sprite frogSprite;
+
+
+      public FrogObject(){
+        abilityOne = "Attack";
+        abilityTwo = "Defend";
+        abilityThree = "";
+        abilityFour = "";
+        
+        
+      }
+
+    }
