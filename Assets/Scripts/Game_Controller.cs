398481776e6d8cdb1c05f68d16c673a3604246e3
Mark Heinze <mheinze@Marks-MacBook-Pro-2.local>
2023-11-29 22:03:34 -0500

    Finish up with the asset

--- Assets/Scripts/Game_Controller.cs
+++ Assets/Scripts/Game_Controller.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
-using Frog;
 
 public class Game_Controller : MonoBehaviour{
 
--- Packages/edu.jheinze/Assets/Frog.cs
+++ Packages/edu.jheinze/Assets/Frog.cs
@@ -1,3 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
     public class FrogObject{
       public string name;
       public string abilityOne;
